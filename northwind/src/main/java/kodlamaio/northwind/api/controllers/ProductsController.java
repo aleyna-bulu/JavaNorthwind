eca8973faed51ac37ccb73971e849053d3eebb99
CASPER <CASPER@CASPER-PC>
2021-06-09 17:40:30 +0300

    .

--- northwind/src/main/java/kodlamaio/northwind/api/controllers/ProductsController.java
+++ northwind/src/main/java/kodlamaio/northwind/api/controllers/ProductsController.java
@@ -3,6 +3,7 @@ package kodlamaio.northwind.api.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,6 +21,7 @@ import kodlamaio.northwind.entities.dtos.ProductWithCategoryDto;
 @RestController
 @RequestMapping("/api/products")
 //dış dünyadan istekte bulunurlarsa bu controller cevap verecek
+@CrossOrigin
 public class ProductsController {
 
 	@Autowired
